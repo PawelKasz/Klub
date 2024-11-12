32a41306500ed669c6187d359a9fc8bf93063755
XaVeRski <marcin.xaver@gmail.com>
2024-11-12 11:17:51 +0100

    Nic nie robimy gdy wychyl_lotki jest zero

--- plat.cpp
+++ plat.cpp
@@ -18,10 +18,7 @@ short plat::oblicza_kon( int wychyl_lotki, int kont_natarcia ){
     {
         kont_natarcia = kont_natarcia - wychyl_lotki;
     }
-    else if(wychyl_lotki == 0)
-    {
-        kont_natarcia = kont_natarcia - wychyl_lotki;
-    }
+  
     return kont_natarcia;
 }
 
